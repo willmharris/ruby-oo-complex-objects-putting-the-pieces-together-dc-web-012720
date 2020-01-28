3a4a56f9986f845211e5266317871385cdd7a37c
will <willmharris0@gmail.com>
2020-01-28 14:54:27 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
 
-
+mybook = Book.new("Excelent Title")
+mybook.turn_page  #=> flipping ect
+mybook.genre = "Fantasy"
+puts mybook.genre # "Fantasy"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    def initialize(brand_input)
+        @brand = brand_input
+    end 
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+end 
